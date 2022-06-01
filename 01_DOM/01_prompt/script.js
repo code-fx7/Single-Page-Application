3d46785082fd15fac17e81132c6947a33600761a
code-fx <code-fx@mail.de>
2022-06-01 16:24:16 +0200

    update 01_prompt

--- 01_DOM/01_prompt/script.js
+++ 01_DOM/01_prompt/script.js
@@ -1,2 +1,8 @@
 // Frage an Nutzer
-const userName = prompt
+const userName = prompt("Wie heißt du?"); // pause
+
+console.log(`Wilkommen auf meiner Seite ${userName}`);
+
+const favFood = prompt("Welches ist dein Lieblingsessen?"); // pause
+
+console.log(`${userName} liebt ${favFood}`);
