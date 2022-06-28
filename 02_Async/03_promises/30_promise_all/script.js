81ef576f8c7b51be7470e29a7e713213af15fcf2
code-fx <code-fx@mail.de>
2022-06-28 14:44:37 +0200

    edit index.html, script.js in 30_promise_all

--- 02_Async/03_promises/30_promise_all/script.js
+++ 02_Async/03_promises/30_promise_all/script.js
@@ -1,11 +1,25 @@
-const images = document.querySelectorAll("main img");
+const images = [
+  "https://images.pexels.com/photos/12576276/pexels-photo-12576276.jpeg?cs=srgb&dl=pexels-phil-desforges-12576276.jpg&fm=jpg",
+  "https://images.pexels.com/photos/11789773/pexels-photo-11789773.jpeg?cs=srgb&dl=pexels-yuliia-tretynychenko-11789773.jpg&fm=jpg",
+  "https://images.pexels.com/photos/10757791/pexels-photo-10757791.jpeg?cs=srgb&dl=pexels-micaela-alejandro-10757791.jpg&fm=jpg",
+  "htts://images.pexels.com/photos/7913028/pexels-photo-7913028.jpeg?cs=srgb&dl=pexels-alejandro-peralta-7913028.jpg&fm=jpg",
+  "https://images.pexels.com/photos/12132217/pexels-photo-12132217.jpeg?cs=srgb&dl=pexels-malik-skydsgaard-12132217.jpg&fm=jpg",
+];
+
 const loader = document.querySelector("#loader");
 const main = document.querySelector("main");
 
 function loadImage(img) {
-  const imageLoadPromise = new Promise((reslove, reject) => {
-    img.addEventListener("load", resolve);
+  const imageElement = document.createElement("img");
+
+  const imageLoadPromise = new Promise((resolve, reject) => {
+    imageElement.addEventListener("load", resolve);
+    imageElement.addEventListener("error", reject);
   });
+
+  imageElement.src = img;
+
+  main.appendChild(imageElement);
 
   return imageLoadPromise;
 }
@@ -13,17 +27,30 @@ function loadImage(img) {
 checkLoading();
 
 async function checkLoading() {
-  //    [Promise, Promise...]           Promise
-  const imagePromise = Array.from(images).map((img) => loadImage(img));
+  //    [Promise, Promise...]                   Promise
+  const imagePromises = Array.from(images).map((img) => loadImage(img));
 
-  await Promise.all([
+  window.imagePromises = imagePromises;
+
+  await Promise.allSettled([
+    // Polyfill for older browsers is loaded from ./allSettled-polyfill.js
     startTimeout(), // Promise
-    ...imagePromise, // destructuring ...[Promise, Promise]
+    ...imagePromises, // destructuring ...[Promise, Promise]
   ]);
-
   transitionAfterLoading();
 }
 
 function startTimeout() {
-  const timeoutPromise = new Promise((resolve, reject) => {});
+  const timeoutPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 300);
+  });
+
+  return timeoutPromise;
+}
+
+function transitionAfterLoading() {
+  loader.style.display = "none";
+  main.style.opacity = 1;
 }
