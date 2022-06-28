b7584dc2edf6d967ce55631e69f1ebc591d9aaca
code-fx <code-fx@mail.de>
2022-06-28 14:47:42 +0200

    add allSettled-polyfill.js in 30_promise_all

---
+++ 02_Async/03_promises/30_promise_all/allSettled-polyfill.js
@@ -0,0 +1,14 @@
+// Promise.allSettled polyfill https://javascript.info/promise-api#promise-allsettled
+if (!Promise.allSettled) {
+  const rejectHandler = (reason) => ({ status: "rejected", reason });
+
+  const resolveHandler = (value) => ({ status: "fulfilled", value });
+
+  Promise.allSettled = function (promises) {
+    const convertedPromises = promises.map((p) =>
+      Promise.resolve(p).then(resolveHandler, rejectHandler)
+    );
+    return Promise.all(convertedPromises);
+  };
+}
+// Polyfill End
