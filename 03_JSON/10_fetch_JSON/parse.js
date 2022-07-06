d7d98fdb04dc5187bb7a435dea6e5b9be785cbca
code-fx <code-fx@mail.de>
2022-07-06 13:05:43 +0200

    update

--- 03_JSON/10_fetch_JSON/parse.js
+++
@@ -1,8 +0,0 @@
-// In JSON sind Object properties immer in Anführungszeichen
-
-// Im JSON können Objekte und Arrays unbegrenzt verschachtelt werden
-const dataString = '{"firstName":"Chris", "hobbies":["VanLife", "grillen"]}';
-
-const obj = JSON.parse(dataString);
-
-console.log(obj);
