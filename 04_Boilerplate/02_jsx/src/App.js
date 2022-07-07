dd749c8c7bc40ea043f3658d778310b8d1976b8c
code-fx <code-fx@mail.de>
2022-07-07 14:55:42 +0200

    edit App.js in 02_jsx

--- 04_Boilerplate/02_jsx/src/App.js
+++ 04_Boilerplate/02_jsx/src/App.js
@@ -2,23 +2,50 @@ import logo from "./birdy.png";
 import "./App.css";
 import gezwitscher from "./gezwitscher.js";
 
+// ohne map
+
+// function App() {
+//   const post = {
+//     user: "Ferdi Hoske",
+//     content: "Lerne gerade React. Macht voll Spaß!",
+//     date: "10-05-2017 11:01",
+//   };
+
+//   return (
+//     <div className="App">
+//       <img src={logo} className="logo" alt="logo" />
+//       <h1 className="twitter-blue">Zwitscher</h1>
+//       <h2>Posts</h2>
+//       <p>
+//         <b>{post.user}</b>
+//       </p>
+//       <p>{post.content}</p>
+//       <p>{post.date}</p>
+//     </div>
+//   );
+// }
+
+// mit map
+
 function App() {
-  const post = {
-    user: "Ferdi Hoske",
-    content: "Lerne gerade React. Macht voll Spaß!",
-    date: "10-05-2017 11:01",
-  };
+  const posts = gezwitscher.map((post, index) => {
+    return (
+      <div key={index}>
+        <p>
+          <b>{post.user}</b>
+        </p>
+        <p>{post.content}</p>
+        <p>{post.date}</p>
+      </div>
+    );
+  });
 
   return (
     <div className="App">
       <img src={logo} className="logo" alt="logo" />
       <h1 className="twitter-blue">Zwitscher</h1>
       <h2>Posts</h2>
-      <p>
-        <b>{post.user}</b>
-      </p>
-      <p>{post.content}</p>
-      <p>{post.date}</p>
+      {posts}
     </div>
   );
 }
